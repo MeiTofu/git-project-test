b22eca351c0f871dabd427617afadaa85c6a7dac
yjf <409774542@qq.com>
2023-04-07 21:18:04 +0800

    IDEA 添加java文件

---
+++ src/JavaTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author: yjf
+ * @create: 2023/4/7 21:16
+ * @message:
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("hello github world!");
+    }
+}
